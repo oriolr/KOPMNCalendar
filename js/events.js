75b500063bf07cb4625559d039a36ff2c7ce80ec
Rosler <oriolr32@gmail.com>
2018-11-20 22:58:31 -0500

    initial commit

---
+++ js/events.js
@@ -0,0 +1,273 @@
+  $(function() {
+
+            $('#calendar').fullCalendar({
+              events: [
+                        {
+                          title  : 'Toastmasters 9:00 am - 12:00 pm',
+                          start  : '2018-02-03'
+                        },
+
+                        {
+                          title  : 'Real Men Read Delray Beach Library 9:00 am - 10:00 am',
+                          start  : '2018-02-10'
+                        },
+                        {
+                          title  : 'Build a Bus 10:00 am - 12:00 pm Boynton Beach',
+                          start  : '2018-02-10'
+                        },
+                        {
+                          title  : 'Jeans & Lace Fundraiser 6:00 pm - 12:00 am Elks Lodge',
+                          start  : '2018-02-10'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-02-07'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-02-14'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-02-21'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-02-28'
+                        },//End of February
+
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-03-03'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-03-10'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-03-17'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-03-31'
+                        },
+                        {
+                          title  : 'Stream3',
+                          start  : '2018-03-19',
+                          end    : '2018-03-24'
+                        },// End of March
+
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-04-07'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-04-14'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-04-28'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-04-04'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-04-11'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-04-18'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-04-25'
+                        },
+                        {
+                          title  : 'Anti-Bullying Rally - Boynton Beach',
+                          start  : '2018-04-21'
+                        },// End of April
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-05-02'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-05-09'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-05-16'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-05-23'
+                        },
+                        {
+                          title  : 'C4 Connect 6:00 pm - 9:00 pm Center for Creative Education, West Palm Beach',
+                          start  : '2018-05-30'
+                        },
+                        {
+                          title  : 'Crime Prevention Conference - Jacksonville',
+                          start  : '2018-05-31'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-05-05'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-05-12'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-05-19'
+                        },
+                        {
+                          title  : 'Saturday Academy',
+                          start  : '2018-05-26'
+                        },//End of May
+                        {
+                          title  : 'Crime Prevention Conference - Jacksonville',
+                          start  : '2018-06-01',
+                          end    : '2018-06-03'
+                        },
+                        {
+                          title  : 'Stream3',
+                          start  : '2018-06-04',
+                          end    : '2018-06-09'
+                        },
+                        {
+                          title  : 'Stream3',
+                          start  : '2018-06-11',
+                          end    : '2018-06-16'
+                        },
+                        {
+                          title  : 'Stream3',
+                          start  : '2018-06-18',
+                          end    : '2018-06-23'
+                        },
+                        {
+                          title  : 'Sports Camp',
+                          start  : '2018-06-28',
+                          end    : '2018-06-30'
+                        },//End of June
+                        {
+                          title  : 'Sports Camp',
+                          start  : '2018-07-01'
+                        },
+                        {
+                          title  : 'A Quest for Happiness',
+                          start  : '2018-07-02',
+                          end    : '2018-07-03'
+                        },
+                        {
+                          title  : 'A Quest for Happiness',
+                          start  : '2018-07-05',
+                          end    : '2018-07-07'
+                        },
+                        {
+                          title  : 'A Quest for Happiness',
+                          start  : '2018-07-09',
+                          end    : '2018-07-14'
+                        },
+                        {
+                          title  : 'A Quest for Happiness',
+                          start  : '2018-07-16',
+                          end    : '2018-07-21'
+                        },
+                        {
+                          title  : 'A Quest for Happiness',
+                          start  : '2018-07-23',
+                          end    : '2018-07-28'
+                        },
+                        {
+                          title  : 'NOBLE Camp',
+                          start  : '2018-07-28',
+                          end    : '2018-08-02'
+                        },//End of July
+                        //November
+                        {
+                          title  : 'ArtsConnection Center for Creative Education 6:30 pm - 8:30 pm (Middle & High)',
+                          start  : '2018-11-01'
+                        },
+                        {
+                          title  : 'City Commission Monthly Report 6:00 pm - 6:30 pm',
+                          start  : '2018-11-06'
+                        },
+                        {
+                          title  : 'Switch  6:00 pm - 8:00 pm',
+                          start  : '2018-11-07'
+                        },
+                        {
+                          title  : 'ArtsConnection Center for Creative Education 6:30 pm - 8:30 pm (Middle & High)',
+                          start  : '2018-11-08'
+                        },
+                        {
+                          title  : 'Deadline for Dr. MLK Essay Contest',
+                          start  : '2018-11-09'
+                        },
+                        {
+                          title  : 'Oratorical Academy 9-12 noon (All) ',
+                          start  : '2018-11-10'
+                        },
+                        {
+                          title  : 'Spady Museum Family Festival 2:00pm -5:00 pm',
+                          start  : '2018-11-10'
+                        },
+
+                        {
+                          title  : 'Creative Writing workshop 4:30pm - 5:30pm  Library',
+                          start  : '2018-11-12'
+                        },
+                        {
+                          title  : 'Switch  6:00 pm - 8:00 pm',
+                          start  : '2018-11-14'
+                        },
+                        {
+                          title  : 'ArtsConnection Center for Creative Education 6:30 pm - 8:30 pm (Middle & High)',
+                          start  : '2018-11-15'
+                        },
+                        {
+                          title  : 'Go4it Pitch Pratice 9:00am - 10:30am',
+                          start  : '2018-11-17'
+                        },
+                        {
+                          title  : 'Creative Writing workshop 4:30pm - 5:30pm  Library',
+                          start  : '2018-11-19'
+                        },
+                        {
+                          title  : 'City Commission Monthly Report 6:00 pm - 6:30 pm',
+                          start  : '2018-11-20'
+                        },
+                        {
+                          title  : 'Switch  6:00 pm - 8:00 pm',
+                          start  : '2018-11-21'
+                        },
+                        {
+                          title  : 'Kids Fun Festival Intracoastal Park, Boynton Beach  10:00 am - 3:00 pm',
+                          start  : '2018-11-24'
+                        },
+                        {
+                          title  : 'Black Violin, Kravis Center  6:00pm - 9:00 pm',
+                          start  : '2018-11-26'
+                        },
+                        {
+                          title  : 'Switch  6:00 pm - 8:00 pm',
+                          start  : '2018-11-28'
+                        },
+                        {
+                          title  : 'ArtsConnection Center for Creative Education 6:30 pm - 8:30 pm (Middle & High)',
+                          start  : '2018-11-29'
+                        },//end of November
+                        //January
+                        {
+                          title  : '2019 Dr. Martin Luther King, Jr., Essay/Oratorical Contest',
+                          start  : '2019-01-21'
+                        },
+                      ]
+                    });
+        });
